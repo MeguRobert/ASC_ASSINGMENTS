5bd73c5a52dfbf006f5922b2346a31db934de90e
MeguRobi <megurobi14@outlook.hu>
2020-10-30 00:58:59 +0200

    Add: GetData, ConvertFrom and ConvertTo methods

---
+++ Conversie din baza2 in baza putere (4,8,16)/Conversie din baza2 in baza putere (4,8,16)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversie_din_baza2_in_baza_putere__4_8_16_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> base16 = new Dictionary<string, string>() ;
+            base16.Add("0000", "0");
+            base16.Add("0001", "1");
+            base16.Add("0010", "2");
+            base16.Add("0011", "3");
+
+            base16.Add("0100", "4");
+            base16.Add("0101", "5");
+            base16.Add("0110", "6");
+            base16.Add("0111", "7");
+            
+            base16.Add("1000", "0");
+            base16.Add("1001", "1");
+            base16.Add("1010", "2");
+            base16.Add("1011", "3");
+                        
+            base16.Add("1100", "4");
+            base16.Add("1101", "5");
+            base16.Add("1110", "6");
+            base16.Add("1111", "7");
+
+
+            Dictionary<string, string> base8 = new Dictionary<string, string>();
+            base8.Add("000", "0");
+            base8.Add("001", "1");
+            base8.Add("010", "2");
+            base8.Add("011", "3");
+                
+            base8.Add("100", "4");
+            base8.Add("101", "5");
+            base8.Add("110", "6");
+            base8.Add("111", "7");
+
+
+            Dictionary<string, string> base4 = new Dictionary<string, string>();
+            base4.Add("00", "0");
+            base4.Add("01", "1");
+            base4.Add("10", "2");
+            base4.Add("11", "3");
+
+
+
+        }
+    }
+}
--- Conversii_intre_Baze/Conversii_intre_Baze/Program.cs
+++ Conversii_intre_Baze/Conversii_intre_Baze/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -11,23 +12,13 @@ namespace Conversii_intre_Baze
     {
         static void Main(string[] args)
         {
-            int baza , bazaTinta, nr, fractie;
-            string line;
-            
-            Console.WriteLine("Introduceti baza din care doriti sa convertiti numarul");
-            baza = int.Parse(Console.ReadLine());
-            Console.WriteLine("Introduceti bazaTinta in care doriti sa convertiti numarul");
-            bazaTinta = int.Parse(Console.ReadLine());
-
-            //conditii
-            //while(!numarCorect)
-            Console.WriteLine("Introduceti numarul care trebuie convertit");
-            line = Console.ReadLine();
-
+            int baza=0, bazaTinta=0, nr, fractie;
+            string line="", result = "";
+            GetData(ref baza, ref bazaTinta, ref  line);
+ 
             string[] split = line.Split('.');
-            nr = int.Parse(split[0]);
             fractie = 0;
-            bool ok = false;
+            bool ok = false;//?
             if (split.Length <= 2 && split.Length > 0)
             {
 
@@ -39,21 +30,47 @@ namespace Conversii_intre_Baze
             }
             else
             {
-                // Nu e nr corect
+                Console.WriteLine("\n Nu era numar corect!  Va rog introduceti datele din nou! \n");
+                GetData(ref baza, ref bazaTinta, ref line);
+                split = line.Split('.');
             }
+            nr = int.Parse(split[0]);
+            if(baza==10)
+            ConvertFromBase10(nr,baza,bazaTinta,fractie, ref result, split);
+            if (bazaTinta == 10)
+            ConvertToBase10(nr,ref baza,bazaTinta,fractie, ref result, split);
+            Console.WriteLine(result);
+            //  Console.ReadKey();
+        }
+
+        private static void ConvertToBase10(int nr,ref int baza, int bazaTinta, int fractie, ref string result, string[] split)
+        {
+            int i=0, num = 0; ;
+       
+            while (nr>0)
+            {
+                num += nr % 10 * (int)Math.Pow(baza, i);
+                i++;
+                nr /= 10;
+            }
+            Console.WriteLine(num);
+       
+        }
+
+        private static string ConvertFromBase10(int nr,int baza, int bazaTinta, int fractie, ref string result, string[] split)
+        {
 
             Stack<double> stiva = new Stack<double>();
             int cat, rest, numar;
-            string result="";
+            
             string[] hex = { "A", "B", "C", "D", "E", "F" };
             cat = nr;
             numar = nr;
-
             //convertirea unui numar intreg din baza 10 in baza inferior
 
-            if (cat == 0)result += cat;
-            while(cat > 0)
-            {     
+            if (cat == 0) result += cat;
+            while (cat > 0)
+            {
                 //TODO transcriere in metoda recursiva
                 cat = numar / bazaTinta;
                 rest = numar % bazaTinta;
@@ -71,7 +88,7 @@ namespace Conversii_intre_Baze
                 double fr = fractie;
                 numar = split[1].Length;
 
-                while (numar>0)
+                while (numar > 0)
                 {
                     fr /= baza;
                     numar--;
@@ -87,11 +104,34 @@ namespace Conversii_intre_Baze
                 }
                
             }
-
-           
-            Console.WriteLine(result);
-
-          //  Console.ReadKey();
+            return result;
         }
+
+        private static void GetData(ref int baza,ref int bazaTinta,ref string line)
+        {
+            
+            do
+            {
+                Console.WriteLine("Introduceti baza din care doriti sa convertiti numarul");
+                baza = int.Parse(Console.ReadLine());
+            } while (baza < 2 || baza > 16);
+            
+            
+            
+            do
+            {
+                Console.WriteLine("Introduceti bazaTinta in care doriti sa convertiti numarul");
+                bazaTinta = int.Parse(Console.ReadLine());
+            } while (bazaTinta < 2 || bazaTinta > 16);
+            
+
+            //conditii
+            //while(!numarCorect)
+            Console.WriteLine("Introduceti numarul care trebuie convertit");
+            line = Console.ReadLine();
+            if (line[0] == '0') line = line.TrimStart('0');
+            
+        }
+
     }
 }
